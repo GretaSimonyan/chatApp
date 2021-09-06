c27d3d32faabc4cb6d445b3ae4eee1d7ff09b4e5
GretaSimonyan <gsimonyan228@gmail.com>
2021-09-06 18:21:47 +0400

    integrated classnames lib

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,8 +1,9 @@
 import React, { useMemo, memo } from 'react';
 import { useSelector } from 'react-redux';
+import cn from 'classnames';
 import styles from './styles.module.scss';
 
-const getFormatedTime = ({seconds, nanoseconds}) => {
+const getFormatedTime = ({ seconds, nanoseconds }) => {
   if (!seconds) {
     return null
   }
@@ -12,17 +13,19 @@ const getFormatedTime = ({seconds, nanoseconds}) => {
   return `${h < 10 ? '0' : ''}${h}:${m < 10 ? '0' : ''}${m}`;
 };
 
-function Message({message}) {
+function Message({ message }) {
   const formatedTime = useMemo(() => getFormatedTime(message.created), [message.created]);
   const currUser = useSelector(store => store.users.currUser);
   const isMy = currUser?.id === message.senderId;
 
   return (
-    <div className={isMy ? styles.myMessage : styles.message}>
-      <div>
+    <div className={cn(styles.message, {[styles.myMessage]: isMy}) }>
+      <div className={styles.text}>
         {message.text}
       </div>
-      {formatedTime}
+      <div className={styles.time}>
+        {formatedTime}
+      </div>
     </div>
   )
 };
